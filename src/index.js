67403d4268e129b4e0f2e8efc3fff7a85049f0ba
NattReyes <97549723+NattReyes@users.noreply.github.com>
2022-02-09 22:07:04 -0600

    Add files via upload

--- src/index.js
+++ src/index.js
@@ -1,3 +1,22 @@
-import validator from './validator.js';
-
-console.log(validator);
+import  validator from './validator.js';
+var btnsubmit=document.getElementById('submit');
+let Num=document.getElementById('inputNumber');
+//Dar funcionalidad a mi botón, mostrando un mensaje si la tarjeta es válida o inválida
+btnsubmit.addEventListener('click',()=>{
+    var NumTar = Num.value;
+    const validAlert = document.getElementById('validAlert');
+    let name = document.getElementById('name').value;
+        if(validator.isValid(NumTar)===true){ 
+            validAlert.innerText = (name) + "¡TU TARJETA ES VÁLIDA!";
+            validAlert.style.backgroundColor = "#91f769";
+        }else{
+            validAlert.innerText= `${name} ¡TU TARJETA NO ES VÁLIDA!`;
+            validAlert.style.backgroundColor = "#FA7777";
+        }
+  Num.value=validator.maskify(NumTar);
+    })
+//Funcionalidad botón validar otra tarjeta
+    const btnreload = document.getElementById('reload');
+btnreload.addEventListener('click', () => { 
+    location.reload();
+});
